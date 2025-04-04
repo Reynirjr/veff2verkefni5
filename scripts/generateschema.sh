cbbd3ea425666b4b272e9d9cd3acd0edf74e5946
Benjamín <brj46@hi.is>
2025-04-04 22:55:04 +0000

    how about now?

--- scripts/generateschema.sh
+++
@@ -1,7 +0,0 @@
-export $(grep -v '^#' .env.local | xargs)
-
-echo "Generating schema using token: $DATOCMS_PUBLISHED_CONTENT_CDA_TOKEN"
-
-gql.tada generate schema https://graphql.datocms.com \
-  --header "X-Exclude-Invalid: true" \
-  --header "Authorization: $DATOCMS_PUBLISHED_CONTENT_CDA_TOKEN"
